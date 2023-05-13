b6b1edefc43d799fb869b3ee31b05ca81a7fb1b3
DrewnioQ <debski.dev@gmail.com>
2023-05-14 01:10:51 +0200

    Change API calls from useCallback to arrow functions

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useState } from "react"
+import { useEffect, useState } from "react"
 import getData, { getCategories } from "../../lib/ChuckNorrisAPI.ts"
 import { CategoriesAPI, DataAPI } from "../../types/types.ts"
 import Button from "./Button/Button.tsx"
@@ -13,17 +13,13 @@ export default function Card() {
     CategoriesAPI[0] | undefined
   >()
 
-  const handleGetQuoteCallback = useCallback(() => {
-    handleGetQuote()
-  }, [])
-
-  async function handleGetQuote() {
-    const data = await getData()
+  const handleGetQuote = async (category?: string) => {
+    const data = await getData(category)
     if (!data) return
     setResult(data)
   }
 
-  async function handleGetCategories() {
+  const handleGetCategories = async () => {
     const data = await getCategories()
     if (!data) return
     setCategories(data)
