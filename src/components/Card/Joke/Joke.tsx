b31d2cbfbc94e727b48b7333f6ff004d9d97b9ae
DrewnioQ <debski.dev@gmail.com>
2023-05-14 16:27:59 +0200

    Add local Roboto font and change bold to medium

--- src/components/Card/Joke/Joke.tsx
+++ src/components/Card/Joke/Joke.tsx
@@ -3,7 +3,7 @@ import { JokeProps } from "../../../types/types.js"
 export default function Joke({ joke }: JokeProps) {
   return (
     <div className="my-6 min-h-[4rem]">
-      <blockquote className="text-xl font-bold italic text-darkblue-500">
+      <blockquote className="text-xl font-medium italic text-darkblue-500">
         &quot;{joke}&quot;
       </blockquote>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
   content: ["./*.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: "Roboto, sans-serif",
+      },
       colors: {
         darkblue: {
           100: "#d7d7db",
