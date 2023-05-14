11ce5a9a487660177a607d436116b7cf6548f164
DrewnioQ <debski.dev@gmail.com>
2023-05-14 09:58:52 +0200

    Add keyboard accessibility to Select component

--- src/components/Card/Select/Select.tsx
+++ src/components/Card/Select/Select.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-static-element-interactions */
@@ -7,7 +8,7 @@ import {
   ChevronUpIcon,
   XMarkIcon,
 } from "@heroicons/react/24/outline"
-import { useState } from "react"
+import { KeyboardEvent, useEffect, useRef, useState } from "react"
 import { SelectProps } from "../../../types/types.ts"
 
 function capitalizeFirstLetter(string?: string) {
@@ -16,8 +17,10 @@ function capitalizeFirstLetter(string?: string) {
 }
 
 export default function Select({ options, value, onChange }: SelectProps) {
-  const [isOpen, setIsOpen] = useState(false)
-  const [isCategorySelected, setIsCategorySelected] = useState(false)
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [isCategorySelected, setIsCategorySelected] = useState<boolean>(false)
+  const [highlightedIndex, setHighlightedIndex] = useState<number>(0)
+  const containerRef = useRef<HTMLDivElement>(null)
 
   const activeDropdown = isOpen ? "" : "hidden"
   const activeClear = isCategorySelected ? "" : "hidden"
@@ -29,11 +32,18 @@ export default function Select({ options, value, onChange }: SelectProps) {
     ? capitalizeFirstLetter(value)
     : "Select category"
 
-  function handleSelection(event: React.MouseEvent, selection: string) {
+  function handleSelection(
+    event: React.MouseEvent | React.KeyboardEvent,
+    selection: string
+  ) {
     event.stopPropagation()
-    if (selection !== value) onChange(selection)
+    if (selection !== value) selectOption(selection)
     setIsCategorySelected(true)
     setIsOpen(false)
+  }
+
+  function selectOption(option: string) {
+    onChange(option)
   }
 
   function handleClearSelection(event: React.MouseEvent) {
@@ -43,13 +53,55 @@ export default function Select({ options, value, onChange }: SelectProps) {
     setIsOpen(false)
   }
 
+  useEffect(() => {
+    if (isOpen) setHighlightedIndex(0)
+  }, [isOpen])
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.target !== containerRef.current) return
+      switch (e.code) {
+        case "Enter":
+        case "Space":
+          setIsOpen((prev) => !prev)
+          break
+
+        case "ArrowUp":
+        case "ArrowDown": {
+          if (!isOpen) {
+            setIsOpen(true)
+            break
+          }
+
+          const newValue = highlightedIndex + (e.code === "ArrowDown" ? 1 : -1)
+          if (newValue >= 0 && newValue < options.length) {
+            setHighlightedIndex(newValue)
+          }
+          break
+        }
+        case "Escape":
+          setIsOpen(false)
+          break
+        default:
+          break
+      }
+    }
+
+    containerRef.current?.addEventListener("keydown", handler)
+
+    return () => {
+      containerRef.current?.removeEventListener("keydown", handler)
+    }
+  }, [isOpen, highlightedIndex, options])
+
   return (
     <div
-      className={`relative my-4 flex items-center gap-2 rounded-md border-2 p-2 text-neutral-600 outline-none ${activeSelect}
+      ref={containerRef}
+      className={`select relative my-4 flex items-center gap-2 rounded-md border-2 p-2 text-neutral-600 outline-none focus-visible:outline-darkblue-500 ${activeSelect}
       ${catSelSelect}`}
       tabIndex={0}
       onClick={() => setIsOpen((prev) => !prev)}
-      onBlur={() => setIsOpen(false)}
+      // onBlur={() => setIsOpen(false)}
     >
       <span
         className={`mx-2 flex-grow ${
@@ -82,14 +134,17 @@ export default function Select({ options, value, onChange }: SelectProps) {
         )}
       </button>
       <ul
-        className={`absolute -left-[1px] top-full z-50 mb-2 max-h-32 w-[calc(100%+3px)] overflow-y-auto rounded-md rounded-t-none border-2 border-t-0 bg-white ${catSelSelect} ${activeDropdown}`}
+        className={`absolute -left-[1px] top-full z-50 mb-2 max-h-32 w-[calc(100%+3px)] overflow-y-auto rounded-md rounded-t-none border-2 border-t-0 bg-white focus-visible:border-darkblue-500 ${catSelSelect} ${activeDropdown}`}
       >
         <div className="mb-2">
-          {options.map((option) => (
+          {options.map((option, idx) => (
             <li
-              key={option}
-              tabIndex={0}
-              className="mx-2 cursor-pointer rounded-md px-2 py-4 hover:bg-neutral-200 hover:font-bold hover:text-darkblue-500"
+              key={idx}
+              className={`mx-2 cursor-pointer rounded-md px-2 py-4 hover:bg-neutral-200 hover:font-bold hover:text-darkblue-500 ${
+                idx === highlightedIndex ? "bg-neutral-200 font-bold" : ""
+              }`}
+              onMouseEnter={() => setHighlightedIndex(idx)}
+              onKeyDown={(e) => handleSelection(e, option)}
               onClick={(e) => {
                 handleSelection(e, option)
               }}
